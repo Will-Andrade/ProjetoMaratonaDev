a68f72b17ea40456c98d204bfa82758be5db4e8b
Willian Andrade <willian.santos.andra@outlook.com>
2020-02-20 15:49:50 -0400

    Adicionada toda a lógica de conexão da aplicação com o banco de dados PostgreSQL criado

--- server.js
+++ server.js
@@ -3,14 +3,6 @@ const express = require("express")
 const server = express()
 
 
-// Configurando a engine de templates
-const nunjucks = require("nunjucks")
-nunjucks.configure("./", {
-    express: server,
-    noCache: true,
-})
-
-
 // Configurando o servidor para apresentar os arquivos estáticos (js/css/imgs/etc)
 server.use(express.static('public'))
 
@@ -19,9 +11,34 @@ server.use(express.static('public'))
 server.use(express.urlencoded({ extendend: true}))
 
 
+// Habilitando a conexão com o DB
+const Pool = require('pg').Pool
+const db = new Pool({
+    user: 'postgres',
+    password: '000000',
+    host: 'localhost',
+    port: 5432,
+    database: 'plataformadoe',
+})
+
+
+// Configurando a engine de templates
+const nunjucks = require("nunjucks")
+nunjucks.configure("./", {
+    express: server,
+    noCache: true,
+})
+
+
 // Configurando a apresentação da página
 server.get("/", function(req, res) {
-    return res.render("./index.html", { donors })
+    
+    db.query("SELECT * FROM donors", function(err, result){
+        if (err) return res.send("Erro de Banco de Dados!!")
+
+        const donors = result.rows 
+        return res.render("./index.html", { donors })
+    })
 })
 
 server.post("/", function(req, res){
@@ -29,37 +46,30 @@ server.post("/", function(req, res){
     const email = req.body.email
     const blood = req.body.blood
 
-    donors.push({
-        name: name,
-        blood: blood
+    if (name == "" || email == "" || blood == "")
+    {
+        return res.send("Todos os campos são obrigatórios!")
+    }
+    
+    // Inserindo values dentro do DB
+    const query = `
+        INSERT INTO donors ("name", "email", "blood") 
+        VALUES ($1, $2, $3)
+    `
+
+    const values  = [name, email, blood]
+
+    db.query(query, values, function(err){
+        if (err) return res.send("Erro no Banco de Dados!!")
+
+        return res.redirect("/")
     })
 
-    return res.redirect("/")
 })
 
 
 // Iniciar o servidor e permitir acesso na porta 3000
 server.listen(3000, function() {
-    console.log ("Server initiated.")
+    console.log("Server initiated.")
 })
-
-// Lista de doadores
-const donors = [
-    {
-        name: "Gustavo Lima",
-        blood: "AB+",
-    },
-    {
-        name: "Mateus Brandao",
-        blood: "A+",
-    },
-    {
-        name: "Thalys Matias",
-        blood: "O+",
-    },
-    {
-        name: "Andre Bastos",
-        blood: "A-",
-    },
-]
 
